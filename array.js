1446cccf40440668e4a3fc017da05b308cac5e22
MinhDo0210 <minhdohungkhanh@gmail.com>
2021-12-21 17:13:30 +0700

    update Array

--- array.js
+++ array.js
@@ -1,13 +1,15 @@
 const array = [1, 3, 6, 9, 5, 7, 12, 20]
-//filter
+//filter: lặp các phần tử trong mảng và trả về một mảng mới thỏa mãn điều kiện lọc
 let result = array.filter(array => array < 5);
 console.log(result);
-//reduce
-console.log(array.reduce((acc, ele) => {
-  return acc + ele
-},0));
-//map
+
+//map: Tạo một mảng mới thay đổi phần tử theo điều kiện
 const result1 = array.map((val) => {
   return val*2
 })
 console.log(result1);
+
+//reduce: 
+console.log(array.reduce((acc, ele) => {
+  return acc + ele
+},0));
