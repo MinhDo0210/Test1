c78b3d1f19c26814f71e3de0d4e21b7735709ea5
MinhDo0210 <minhdohungkhanh@gmail.com>
2021-12-22 14:58:44 +0700

    update reduce

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 const array = [1, 3, 6, 9, 5, 7, 12, 20]
+
 //filter(): lặp các phần tử trong mảng và trả về một mảng mới thỏa mãn điều kiện lọc
 let result = array.filter(array => array < 5);
 console.log(result);
@@ -15,11 +16,63 @@ console.log(result1);
 let result2 = array.find(array => array > 5);
 console.log(result2);
 
-//findIndex(): Trả về giá trị Index của một phần tử đầu tiên trong mảng thõa mãn điều kiện tìm kiếm.
+//findIndex(): Trả về giá trị Index (chỉ mục) của một phần tử đầu tiên trong mảng thõa mãn điều kiện tìm kiếm.
 let result3 = array.findIndex(array => array > 8);
 console.log(result3);
 
+
 //reduce(): dùng để tính toán từ các phần tử của mảng (theo thứ tự từ trái sang phải).
+//Accumulator: biến lưu trữ
+//Current Value: Giá trị hiện tại
+//initial value: giá trị khởi tạo
+
 console.log(array.reduce((acc, ele) => {
   return acc + ele
-},0));
+},0)); //initial value
+
+//Flat - "làm phẳng" mảng từ Mảng sâu (đưa về mảng cùng cấp)
+var deptArray = [1, 2, [3, 4], 5, 6, [7, 8, 9]];
+
+var flatArray = deptArray.reduce((flatOutput, deptItem) => {
+  return flatOutput.concat(deptItem); //concat: nối mảng
+}, []) //initial value: mảng
+console.log(flatArray);
+
+var topics = [
+  {
+    topic: "Font-end",
+    courses: [
+      {
+        id: 1,
+        title: "HTML, CSS"
+      },
+      {
+        id: 2,
+        title: "JavaScript"
+      },
+      {
+        id: 3,
+        title: "ReactJS"
+      }
+    ]
+  },
+  {
+    topic: "Back-end",
+    courses: [
+      {
+        id: 1,
+        title: "PHP"
+      },
+      {
+        id: 2,
+        title: "NodeJS"
+      },
+    ]
+  },
+]
+
+var newCourses = topics.reduce((course, topic) => {
+  return course.concat(topic.courses);
+}, []);
+
+console.log(newCourses);
